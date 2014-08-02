cfe5129812f8f394b4f823a735307a6182ec5438
Colin Alston <colin.alston@gmail.com>
2014-08-02 18:59:00 +0200

    Make our UI code neater

--- main.py
+++ main.py
@@ -6,6 +6,40 @@ import pygame
 from pygame.locals import *
 from camz import surface, camera
 
+class Button(object):
+    def __init__(self, text, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.text = surf.font.render(text, False, colorWhite)
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, colorWhite,
+            (self.x, self.y, self.w, self.h), 2)
+        surface.blit(self.text, (self.x+15, self.y+12))
+
+    def inside(self, x, y):
+        inx = (x > self.x) and (y < (self.x+self.w))
+        iny = (y > self.y) and (y < (self.y+self.h))
+
+        if inx and iny:
+            return True
+        else:
+            return False
+
+    def sendEvent(self, event, surface):
+        if event.type == MOUSEBUTTONDOWN:
+            x, y = event.pos
+
+            if self.inside(x, y):
+                pygame.draw.rect(surface, colorBlack, 
+                    (self.x, self.y, self.w, self.h), 2)
+                pygame.display.flip()
+                return True
+
+        return False
+
 
 surf = surface.FBSurface()
 camera = camera.Camera()
@@ -17,44 +51,32 @@ running = True
 colorWhite = (255,255,255)
 colorBlack = (0,0,0)
 
-store = surf.font.render('SAVE', False, colorWhite)
-irnv = surf.font.render('IRNV', False, colorWhite)
+store = Button('SAVE', 2, 200, 60, 40)
+irnv = Button('IRNV', 258, 200, 60, 40)
+
+buttons = [store, irnv]
 
 while running:
     cam = camera.captureStream()
 
-    text = surf.font.render(time.ctime(), False, colorWhite)
-    
-    cam.blit(text, (1,1))
+    uitime = surf.font.render(time.ctime(), False, colorWhite)
+    cam.blit(uitime, (1,1))
 
-    # Save button
-    pygame.draw.rect(cam, colorWhite, (2, 200, 60, 40), 2)
-    cam.blit(store, (17, 212))
-
-    # IRNV button
-    pygame.draw.rect(cam, colorWhite, (258, 200, 60, 40), 2)
-    cam.blit(irnv, (273, 212))
+    for button in buttons:
+        button.draw(cam)
 
     for event in pygame.event.get():
         if event.type in (QUIT, KEYDOWN):
             running = False
 
-        if event.type == MOUSEBUTTONDOWN:
-            mx, my = event.pos
-            
-            if (mx > 2) and (mx <62) and (my > 200) and (my < 240):
-                pygame.draw.rect(cam, colorBlack, (2, 200, 60, 40), 2)
-                surf.display.blit(cam, (0,0))
-                pygame.display.flip()
-                camera.writeStream('%s.h264' % time.strftime('%d-%m-%Y-%H%M%S'))
+        if store.sendEvent(event, cam):
+            camera.writeStream('%s.h264' % time.strftime('%d-%m-%Y-%H%M%S'))
 
-            if (mx > 258) and (mx <320) and (my > 200) and (my < 240):
-                pygame.draw.rect(cam, colorBlack, (258, 200, 60, 40), 2)
-                if camera.irnv:
-                    camera.irnvOff()
-                else:
-                    camera.irnvOn()
-
+        if irnv.sendEvent(event, cam):
+            if camera.irnv:
+                camera.irnvOff()
+            else:
+                camera.irnvOn()
 
     surf.display.blit(cam, (0,0))
     pygame.display.flip()
